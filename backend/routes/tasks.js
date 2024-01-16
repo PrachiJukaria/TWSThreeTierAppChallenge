fd0ffff936bec61e36dc35cf693f8a6189ccc6af
PrachiJukaria <74362259+PrachiJukaria@users.noreply.github.com>
2024-01-16 16:25:28 +0530

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -14,7 +14,6 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
     try {
         const tasks = await Task.find();
-        // chk
         res.send(tasks);
     } catch (error) {
         res.send(error);
